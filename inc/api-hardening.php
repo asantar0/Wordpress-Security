0d69923470dc4a415fba88074a1b1fa8d2363a34
Agustin Santarelli <agustins@root-view.com>
2025-03-06 08:17:40 -0300

    First commit. Folder 'inc', file api-hardening.php and file functions.php created

---
+++ inc/api-hardening.php
@@ -0,0 +1,14 @@
+/*
+ * It's used the public IP "177.178.179.180" in order to give an example.
+ */
+
+function restrict_rest_api_to_localhost() {
+
+	$whitelist = [ '127.0.0.1', "::1", "177.178.179.180"];
+
+	if( ! in_array($_SERVER['REMOTE_ADDR'], $whitelist ) ){
+		die( 'REST API is disabled.' );
+	}
+}
+
+add_action( 'rest_api_init', 'restrict_rest_api_to_localhost', 0 );
